2dd5b52b9c000f056c42c4ecf23fd4039b3952b9
gasyo <gasyo@DESKTOP-MC89CKM>
2017-12-27 05:38:25 +0900

    ログ

---
+++ README.txt
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+Log4j2
+
+- Download
+https://logging.apache.org/log4j/2.x/download.html
+log4j-api-2.10.0.jar
+log4j-core-2.10.0.jar
+
+- Configuration
+http://logging.apache.org/log4j/2.x/manual/configuration.html
+
+- Sample
+https://itsakura.com/java-log4j2-sample
+--------------------------------------------------------
---
+++ WEB-INF/src/java/common/filter/RequestLogFilter.java
@@ -0,0 +1,34 @@
+package common.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class RequestLogFilter implements javax.servlet.Filter {
+
+	private static final Logger logger = LogManager.getLogger(RequestLogFilter.class);
+
+	private FilterConfig filterConfig;
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws java.io.IOException, javax.servlet.ServletException {
+
+		logger.info("start", "");
+
+		chain.doFilter(request, response);
+
+		logger.info("finish", "");
+	}
+
+	public void init(final FilterConfig filterConfig) {
+		this.filterConfig = filterConfig;
+	}
+
+	public void destroy() {
+		filterConfig = null;
+	}
+}
